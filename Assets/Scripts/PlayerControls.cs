40ea5fdd2701055906ebbd44187d298988664537
IanMartinezPicazo <iama249@vidalibarraquer.net>
2025-09-18 09:37:13 +0200

    Win condition + Fixed player movement (still not a tridecagon)

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
@@ -6,7 +8,10 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     private float movementX;
     private float movementY;
-    [SerializeField] private float speed = 5;
+    [SerializeField] private float speed = 20;
+
+    [SerializeField] private TextMeshProUGUI ñamsText;
+    private int ñams = 0;
 
     private void FixedUpdate()
     {
@@ -19,5 +24,20 @@ public class PlayerControls : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Collectable"))
+        {
+            Destroy(other.gameObject);
+            ñams++;
+            ñamsText.text = $"Ñams: {ñams}";
+
+            if (ñams == 8)
+            {
+                SceneManager.LoadScene("BigWin");
+            }
+        }
     }
 }
